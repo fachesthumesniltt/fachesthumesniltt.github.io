8d5d4e1c552c17480be834ef48be67cc49206e62
jhanos <thomas@thonis.eu>
2020-01-05 21:49:01 +0000

    add somes files

---
+++ facebook.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+json_data=$(curl -X GET  "https://graph.facebook.com/1909136939359253/posts?access_token=$token&limit=15" | jq '.data[] | select(.message!=null)| {id: .id,message: .message, date: .created_time}')
+for id in $(echo $json_data | jq -r '.id')
+do
+  if [ ! -f "content/post/$id.md" ];then
+    echo -ne "---\ntitle: " > content/post/$id.md
+    echo $json_data | jq -r ". | select(.id==\"$id\") | .message" | head -n 1 >> content/post/$id.md
+    echo -ne "date: " >> content/post/$id.md
+    echo $json_data | jq -r ". | select(.id==\"$id\") | .date" >> content/post/$id.md
+    echo '---' >> content/post/$id.md
+    echo $json_data | jq -r ". | select(.id==\"$id\") | .message" | tail +2 >> content/post/$id.md
+  fi
+done
