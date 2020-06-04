9312e2b6f4ab6f14041cd837b8cc6e6843b480fe
fachesthumesniltt <57316720+fachesthumesniltt@users.noreply.github.com>
2020-06-04 23:30:20 +0200

    Update generateSite.sh

--- generateSite.sh
+++ generateSite.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
-set -x
+set -e
 
 json_data=$(curl -X GET  "https://graph.facebook.com/1909136939359253/posts?access_token=$facebook_token&limit=15" | jq '.data[] | select(.message!=null)| {id: .id,message: .message, date: .created_time}')
 for id in $(echo $json_data | jq -r '.id')
 do
   if [ ! -f "content/post/$id.md" ];then
-    echo -ne "---\ntitle: " > content/post/$id.md
-    echo $json_data | jq -r ". | select(.id==\"$id\") | .message" | head -n 1 >> content/post/$id.md
-    echo -ne "date: " >> content/post/$id.md
+    echo -ne "---\ntitle: \"" > content/post/$id.md
+    echo $json_data | jq -r ". | select(.id==\"$id\") | .message" | head -n 1 | tr -d '\n' >> content/post/$id.md
+    echo -ne "\"\ndate: " >> content/post/$id.md
     echo $json_data | jq -r ". | select(.id==\"$id\") | .date" >> content/post/$id.md
     echo '---' >> content/post/$id.md
     echo $json_data | jq -r ". | select(.id==\"$id\") | .message" | tail +2 >> content/post/$id.md
+    echo "####$id"
+    cat content/post/$id.md
   fi
 done
 hugo
