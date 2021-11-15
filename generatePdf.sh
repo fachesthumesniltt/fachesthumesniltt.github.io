baccde13325cb72a4546de445aa2a72a38f51fc5
jhanos <jhanosl@gmail.com>
2021-11-15 23:00:35 +0100

    add pdf

--- generatePdf.sh
+++ generatePdf.sh
@@ -5,11 +5,13 @@ set -e
 for year in $(ls /app/docs/)
 do
   mkdir -p /app/public/docs/$year/
+  echo "<html><body>" > /app/public/docs/$year/index.html
   for file in $(ls /app/docs/$year/*.md)
   do 
     filename=$(basename $file)
     filePDF=$(echo $filename | sed 's/md$/pdf/g')
     pandoc -s /app/docs/$year/$filename -o /app/public/docs/$year/$filePDF
-    echo "$filePDF" >> /app/public/docs/$year/index.html
+    echo "<a href=\"$filePDF\">$filePDF</a>" >> /app/public/docs/$year/index.html
   done
+  echo "</body></html>" >> /app/public/docs/$year/index.html
 done
