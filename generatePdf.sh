f86cc3f28161d0e49c79669339558174808b24fe
Thomas Langlois <jhanosl@gmail.com>
2022-01-22 15:18:25 +0100

    add cr

--- generatePdf.sh
+++ generatePdf.sh
@@ -11,7 +11,7 @@ do
     filename=$(basename $file)
     filePDF=$(echo $filename | sed 's/md$/pdf/g')
     pandoc -s /app/docs/$year/$filename -o /app/public/docs/$year/$filePDF
-    echo "<a href=\"$filePDF\">$filePDF</a>" >> /app/public/docs/$year/index.html
+    echo "<a href=\"$filePDF\">$filePDF</a></br>" >> /app/public/docs/$year/index.html
   done
   echo "</body></html>" >> /app/public/docs/$year/index.html
 done
